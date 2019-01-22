00b7180dee1b8ec1a4122e7fca77f1619cc9a309
nicolas.seibert <zeibou@gmail.com>
2019-01-21 23:18:55 -0500

    add get_all_videos

--- MultiFilesReader.py
+++ MultiFilesReader.py
@@ -10,7 +10,16 @@ class HiLightDescriptor:
     local_time: timedelta
     global_time: timedelta
 
-def parse_directory(folder, startswith='G', endswith='.mp4'):
+
+@dataclass
+class VideoDescriptor:
+    name : str
+    path: str
+    previous_name : str
+    total_time: timedelta
+
+
+def get_all_hilights(folder, startswith='G', endswith='.mp4'):
     global_time = timedelta(0)
     for filepath in sorted(os.listdir(folder)):
         if filepath.lower().startswith(startswith.lower()) and filepath.lower().endswith(endswith.lower()):
@@ -23,8 +32,19 @@ def parse_directory(folder, startswith='G', endswith='.mp4'):
             global_time += timedelta(seconds=duration)
 
 
+def get_all_videos(folder, startswith='G', endswith='.mp4'):
+    previous_name = None
+    for file_name in sorted(os.listdir(folder)):
+        if file_name.lower().startswith(startswith.lower()) and file_name.lower().endswith(endswith.lower()):
+            full_path = os.path.join(folder, file_name)
+            duration = VideoFileClip(full_path).duration
+            yield VideoDescriptor(file_name, full_path, previous_name, duration)
+            previous_name = file_name
+
 
 if __name__ == '__main__':
     path = "/Users/nicolas.seibert/Documents/foot/2019-01-14/"
-    for d in parse_directory(path, endswith='.MP4'):
+    for d in get_all_videos(path, endswith='.MP4'):
+        print(d)
+    for d in get_all_hilights(path, endswith='.MP4'):
         print(d.path, d.local_time, d.global_time)
